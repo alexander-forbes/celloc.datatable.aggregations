3373c8747e3d1a1a8d347d4162ef48cccd3c5714
Sydney Du Plooy <DuPlooySy@aforbes.co.za>
2018-05-22 17:37:15 +0200

    Implements the Min aggregation.

--- Celloc.DataTable.Aggregations/MinMaxAggregation.cs
+++ Celloc.DataTable.Aggregations/MinMaxAggregation.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Linq;
 
@@ -6,35 +7,6 @@ namespace Celloc.DataTable.Aggregations
 {
 	public static class MinMaxAggregation
 	{
-		public static DataRow Max<T>(this System.Data.DataTable dataTable, ((int Column, int Row), (int Column, int Row)) range) 
-			where T : struct
-		{
-			ArgumentGuards.GuardAgainstNullDataTable(dataTable);
-			ArgumentGuards.GuardAgainstMultipleColumns(range);
-
-			if (!dataTable.Contains(range))
-				return null;
-
-			var column = range.Item1.Column;
-			var maxRowIndex = range.Item1.Row;
-
-			var max = default(T);
-
-			for(var row = range.Item1.Row; row <= range.Item2.Row; row++)
-			{
-				var value = dataTable.GetValue((column, row));
-				var lhs = TypeChanger.ChangeType<T>(value);
-
-				if (Operators.GreaterThan(lhs, max))
-				{
-					max = lhs;
-					maxRowIndex = row;
-				}
-			}
-
-			return dataTable.Rows[maxRowIndex];
-		}
-
 		public static DataRow Max<T>(this System.Data.DataTable dataTable, string range)
 			where T : struct
 		{
@@ -44,6 +16,58 @@ namespace Celloc.DataTable.Aggregations
 			var rangeTuple = dataTable.TranslateRange(range);
 
 			return rangeTuple.HasValue ? Max<T>(dataTable, rangeTuple.Value) : null;
+		}
+
+		public static DataRow Max<T>(this System.Data.DataTable dataTable, ((int Column, int Row), (int Column, int Row)) range)
+			where T : struct
+		{
+			return ApplyOperator<T>(dataTable, range, Operators.GreaterThan);
+		}
+
+		public static DataRow Min<T>(this System.Data.DataTable dataTable, string range)
+			where T : struct
+		{
+			ArgumentGuards.GuardAgainstNullDataTable(dataTable);
+			ArgumentGuards.GuardAgainstNullRange(range);
+
+			var rangeTuple = dataTable.TranslateRange(range);
+
+			return rangeTuple.HasValue ? Min<T>(dataTable, rangeTuple.Value) : null;
+		}
+
+		public static DataRow Min<T>(this System.Data.DataTable dataTable, ((int Column, int Row), (int Column, int Row)) range)
+			where T : struct
+		{
+			return ApplyOperator<T>(dataTable, range, Operators.LessThan);
+		}
+
+		private static DataRow ApplyOperator<T>(System.Data.DataTable dataTable, ((int Column, int Row), (int Column, int Row)) range, Func<T, T, bool> @delegate) 
+			where T : struct
+		{
+			ArgumentGuards.GuardAgainstNullDataTable(dataTable);
+			ArgumentGuards.GuardAgainstMultipleColumns(range);
+
+			if (!dataTable.Contains(range))
+				return null;
+
+			var column = range.Item1.Column;
+			var rowIndex = range.Item1.Row;
+
+			var operand = TypeChanger.ChangeType<T>(dataTable.GetValue((column, rowIndex)));
+
+			for(var row = range.Item1.Row; row <= range.Item2.Row; row++)
+			{
+				var value = dataTable.GetValue((column, row));
+				var lhs = TypeChanger.ChangeType<T>(value);
+
+				if (@delegate(lhs, operand))
+				{
+					operand = lhs;
+					rowIndex = row;
+				}
+			}
+
+			return dataTable.Rows[rowIndex];
 		}
 
 		public static IEnumerable<(object, DataRow)> Max<T>(this IEnumerable<DataRowGrouping> dataRowGroupings, int columnIndex) 
@@ -56,31 +80,66 @@ namespace Celloc.DataTable.Aggregations
 
 			foreach(var group in dataRowGroupings)
 			{
-				var maxRow = FindMaxRowInGroup<T>(group, columnIndex);
+				var maxRow = FindRowInGroup<T>(group, columnIndex, Operators.GreaterThan);
 				maxDataRowPerGroup.Add((group.Key, maxRow));
 			}
 
 			return maxDataRowPerGroup;
 		}
 
-		private static DataRow FindMaxRowInGroup<T>(DataRowGrouping group, int columnIndex) where T : struct
+		public static IEnumerable<(object, DataRow)> Min<T>(this IEnumerable<DataRowGrouping> dataRowGroupings, int columnIndex) 
+			where T : struct
 		{
-			var max = default(T);
-			var maxRow = group.First();
+			ArgumentGuards.GuardAgainstNullDataRowGroupings(dataRowGroupings);
+			ArgumentGuards.GuardAgainstInvalidColumnIndex(dataRowGroupings, columnIndex);
+
+			var maxDataRowPerGroup = new List<(object, DataRow)>();
+
+			foreach(var group in dataRowGroupings)
+			{
+				var maxRow = FindRowInGroup<T>(group, columnIndex, Operators.LessThan);
+				maxDataRowPerGroup.Add((group.Key, maxRow));
+			}
+
+			return maxDataRowPerGroup;
+		}
+		
+		public static IEnumerable<(object, DataRow)> ApplyOperator<T>(this IEnumerable<DataRowGrouping> dataRowGroupings, int columnIndex, Func<T, T, bool> @delegate) 
+			where T : struct
+		{
+			ArgumentGuards.GuardAgainstNullDataRowGroupings(dataRowGroupings);
+			ArgumentGuards.GuardAgainstInvalidColumnIndex(dataRowGroupings, columnIndex);
+
+			var maxDataRowPerGroup = new List<(object, DataRow)>();
+
+			foreach(var group in dataRowGroupings)
+			{
+				var maxRow = FindRowInGroup(group, columnIndex, @delegate);
+				maxDataRowPerGroup.Add((group.Key, maxRow));
+			}
+
+			return maxDataRowPerGroup;
+		}
+
+		private static DataRow FindRowInGroup<T>(DataRowGrouping group, int columnIndex, Func<T, T, bool> @delegate) 
+			where T : struct
+		{
+			var operand = TypeChanger.ChangeType<T>(group.First().ItemArray.ElementAt(columnIndex));
+			var dataRow = group.First();
 
 			foreach (var row in group)
 			{
 				var value = row.ItemArray.ElementAt(columnIndex);
 				var lhs = TypeChanger.ChangeType<T>(value);
 
-				if (Operators.GreaterThan(lhs, max))
+				if (@delegate(lhs, operand))
 				{
-					max = lhs;
-					maxRow = row;
+					operand = lhs;
+					dataRow = row;
 				}
 			}
 
-			return maxRow;
+			return dataRow;
 		}
 	}
 }
